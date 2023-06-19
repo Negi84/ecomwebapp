5fae3fe38bd0e0fe049a15987057198f714010c4
negi84 <vipin@mindrops.com>
2023-06-19 23:55:35 +0530

    created modular component

---
+++ src/components/ProductsList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-if="products.length > 0">
+    <ProductsItemsList
+      v-for="product in products"
+      :key="product.id"
+      :product="product"
+    />
+  </div>
+  <p v-else>You haven't added anything to your cart</p>
+</template>
+
+<script>
+import ProductsItemsList from "./ProductsItemsList.vue";
+export default {
+  name: "ProductsList",
+  components: {
+    ProductsItemsList,
+  },
+  props: ["products"],
+};
+</script>
